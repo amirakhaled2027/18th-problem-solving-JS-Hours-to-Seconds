51813fb515aef9fe15f06d6317d9f438ced48629
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-08 02:48:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Write a function that takes an integer hours and converts it to second
+function converting(hours) {
+    var sec = hours * 3600;
+    return sec;
+  }
+  console.log(converting(4));
